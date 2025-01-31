d65d9b6c12caeb3a702f75e4a9679e5e197c1693
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-01-31 13:06:43 +0330

    print added

--- print_board.py
+++ print_board.py
@@ -1,2 +1,44 @@
 # Write your printing of the board functins here
 # Enjoy coding and have fun!⚡️
+#from apply_move import my_row
+#from main import character
+from pprint import pprint
+a = [
+    [0,1,0,0,0,0,0],
+    [0,0,2,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+]
+b=[]
+
+    # color_codes = {
+      
+    #     'red'     : '\u001b[31m',
+    #     'blue_bright'    : '\u001b[34;1m',
+        
+
+    #     'reset' : '\u001b[0m',
+    # }
+
+def print_mode(a):
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if (a[i][j]==0):
+                print(" ", end=" | ")
+            elif (a[i][j]==1):
+                print('\u001b[0m', '\u001b[34;1m',"●",'\u001b[0m', end=" | ", sep='')
+            elif (a[i][j]==2):
+                print('\u001b[0m','\u001b[31m',"●",'\u001b[0m',end=" | ",sep='')
+            
+        print()
+        print("-"*26)
+
+#a = [['#' for i in range(7)] for j in range(6)]
+print(print_mode(a))
+
+
+
+
+
