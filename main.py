4148e2bd2596e6ae0643523dc59ba59555be877e
Nasir <refreshsho@gmail.com>
2025-01-31 12:56:16 +0330

    added

--- main.py
+++ main.py
@@ -4,36 +4,44 @@ from check_board import *
 
 
 
-
+board = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+]
 
 
 def get_input1():
     p1=["p1"]
     player1= int( input("insert a number of row: "))
     p1 = p1.append(player1)
-    app_result = apply_move(p1)
-    print_result = print_board(app_result)
-    print(print_result)
-    check_result = check_board(print_result)
+    app_result = apply_move(p1, board)
+    print_board(app_result)
+    check_result = check_board(board)
     return check_result
 
 def get_input2(check_result):
     p2 = ["p2"]
     player2= int( input("insert a number of row: "))
     p2 = p2.append(player2)
-    app_result = apply_move(p2)
-    print_result = print_board(app_result)
-    print(print_result)
+    app_result = apply_move(p2, board)
+    print_board(app_result)
     check_result = check_board(print_result)
     return check_result
 
 
 def main():
+    print_board(board)
     while True:
-        get_input1()
-
-        if get_input1 == False:
-
-            get_input2()
+        
+        if get_input1() != False:
+            break
+        
+        if get_input2() != False:
+            break
 
 main()
