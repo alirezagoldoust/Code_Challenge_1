b6c09f03ed88f80e4c897ce86cea61dae6930ce4
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-01-31 18:04:10 +0330

    🚀 used dictionary for better perf

--- main.py
+++ main.py
@@ -15,36 +15,24 @@ board = [
 
 
 def get_input1():
-    p1 = ["p1"]
-    player1 = int(input("insert a number of culumn: "))
-    p1 = p1.append(player1)
-    app_result = apply_move(p1, board)
-    """
+    dictionary1 = {"p1": -1}
+    app_result = apply_move(dictionary1, board)
     board = app_result
     print_board(app_result)
     check_result = check_board(board)
     return check_result
-    """
-    return app_result
-    """
+
+
 def get_input2(check_result):
-    p2 = ["p2"]
-    player2 = int(input("insert a number of culumn: "))
-    p2 = p2.append(player2)
-    app_result = apply_move(p2, board)
-    """
+    dictionary2 = {"p2": 1}
+    app_result = apply_move(dictionary2, board)
     board = app_result
     print_board(app_result)
     check_result = check_board(board)
 
     return check_result
-    """
-    """
-
-    return app_result
 
 
-"""
 def main():
     print_board(board)
     while True:
@@ -57,7 +45,3 @@ def main():
 
 
 main()
-"""
-
-
-get_input1()
