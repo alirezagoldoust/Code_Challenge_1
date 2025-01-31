ee872b52ab177ac5d99109b52af7f37e22147c4d
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-01-31 19:41:28 +0330

    🐛 input option was added

--- main.py
+++ main.py
@@ -15,22 +15,35 @@ board = [
 
 
 def get_input1():
-    dictionary1 = {"p1": -1}
+    global board
+    p1_num = input("Please enter number of the column: ")
+    dictionary1 = {"p1": p1_num}
     app_result = apply(dictionary1, board)
     board = app_result
     print_mode(app_result)
     check_result = checked_board(board)
-    return check_result
+    if check_result == 1:
+        print("Winner is Player 1")
+    elif check_result == -1:
+        print("Winner is Player 2")
+    else:
+        print("No winner yet")
 
 
 def get_input2(check_result):
-    dictionary2 = {"p2": 1}
+    global board
+    p2_num = "Please enter number of the column: "
+    dictionary2 = {"p2": p2_num}
     app_result = apply(dictionary2, board)
     board = app_result
     print_mode(app_result)
     check_result = checked_board(board)
-
-    return check_result
+    if check_result == 1:
+        print("Winner is Player 1")
+    elif check_result == -1:
+        print("Winner is Player 2")
+    else:
+        print("No winner yet")
 
 
 def main():
