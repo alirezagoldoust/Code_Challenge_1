054135a4cd3072728200fee52f236ae712c7d616
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-06 20:57:45 +0330

    🐛 board was fixed

--- main.py
+++ main.py
@@ -3,32 +3,31 @@ from print_board import *
 from check_board import *
 
 board = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
 ]
 
 
 def get_input1(board):
     dictionary1 = {"p1": -1}
     board = apply_move(dictionary1, board)
-    print_board(board)
+    print_mode(board)
     return check_board(board)
 
 
 def get_input2(board):
     dictionary2 = {"p2": 1}
     board = apply_move(dictionary2, board)
-    print_board(board)
+    print_mode(board)
     return check_board(board)
 
 
 def main():
-    print_board(board)
+    print_mode(board)
     while True:
         if get_input1(board):
             break
