dfd6becaa23b1f042f56fab2be111e2f16d26a9d
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-06 20:45:09 +0330

    🎉 project completed

--- check_board.py
+++ check_board.py
@@ -1,31 +1,27 @@
-def checked_board(a):
-    rows = len(a)
-    cols = len(a[0])
-
-    for i in range(rows):
-        for j in range(cols):
+def check_board(a):
+    for i in range(6):
+        for j in range(7):
             sliced_list1 = []
             sliced_list2 = []
             sliced_list3 = []
             sliced_list4 = []
 
             for k in range(4):
-                if j + k < cols:
+                if j + k < 7:
                     sliced_list1.append(a[i][j + k])
-                if i + k < rows:
+                if i + k < 6:
                     sliced_list2.append(a[i + k][j])
-                if i + k < rows and j - k >= 0:
+                if i + k < 6 and j - k >= 0:
                     sliced_list3.append(a[i + k][j - k])
-                if i + k < rows and j + k < cols:
+                if i + k < 6 and j + k < 7:
                     sliced_list4.append(a[i + k][j + k])
 
-            if len(set(sliced_list1)) == 1 and sliced_list1[0] != 0:
-                return sliced_list1[0]
-            if len(set(sliced_list2)) == 1 and sliced_list2[0] != 0:
-                return sliced_list2[0]
-            if len(set(sliced_list3)) == 1 and sliced_list3[0] != 0:
-                return sliced_list3[0]
-            if len(set(sliced_list4)) == 1 and sliced_list4[0] != 0:
-                return sliced_list4[0]
+            for sliced_list in [sliced_list1, sliced_list2, sliced_list3, sliced_list4]:
+                if len(sliced_list) == 4 and all(x == 1 for x in sliced_list):
+                    print("Winner is player 2 (1)")
+                    return True
+                elif len(sliced_list) == 4 and all(x == -1 for x in sliced_list):
+                    print("Winner is player 1 (2)")
+                    return True
 
-    return 0
+    return False
